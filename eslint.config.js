bd5bbea904686caf06eb76909390e4adba65ffc4
Johannes Ewald <johannes.ewald@peerigon.com>
2026-02-23 22:09:38 +0100

    Remove files

--- eslint.config.js
+++
@@ -1,5 +0,0 @@
-import typescriptPreset from "@peerigon/configs/eslint/presets/typescript";
-import vitestRules from "@peerigon/configs/eslint/rules/vitest";
-import stylesNoDefaultExport from "@peerigon/configs/eslint/styles/no-default-export";
-
-export default [...typescriptPreset, ...vitestRules, ...stylesNoDefaultExport];
